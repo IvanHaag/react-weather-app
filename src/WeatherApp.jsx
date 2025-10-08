a0246fd87ed0be48674975c98e452c2478d0f7db
Ivan <ivanhaag98@gmail.com>
2025-10-08 12:44:05 +0000

    finished app

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './WeatherApp.css'
+
+export const WeatherApp = () => {
+  const urlBase = "https://api.openweathermap.org/data/2.5/weather"
+  const apiKey = import.meta.env.VITE_API_KEY
+  const diffKelvin = 273.15
+
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null)
+
+  const fetchWeatherData = async() => {
+    try {
+      const response = await fetch(`${urlBase}?q=${city}&appid=${apiKey}&lang=es`)
+      const data = await response.json();
+      setWeatherData(data);
+    } catch(error) {
+      console.error('There was an error', error)
+    }
+  }
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWeatherData()
+  }
+
+  return (
+    <div className='container'>
+      <h1>Aplicación de Clima</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Ingresar ciudad' value={city} onChange={handleCityChange}/>
+        <button>Buscar</button>
+      </form>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <p>La temperatura actual es {Math.floor(weatherData.main.temp - diffKelvin)}°C</p>
+          <p>La codición meteorológica actual es: {weatherData.weather[0].description}</p>
+          <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}`} alt={weatherData.weather[0].description}/>
+        </div>
+      )}
+    </div>
+  )
+}
